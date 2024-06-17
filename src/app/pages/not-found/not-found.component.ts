976c183e892847ec1f6902f4ef4adc43737e75e9
Ibrokhimkhuja <ibrohimechanic@gmail.com>
2024-06-17 20:50:08 +0500

    (feat) add not-found page

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [RouterModule],
+  standalone: true,
+})
+export class NotFoundComponent {}
