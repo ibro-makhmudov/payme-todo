a7fd55a0b560268b2e31bf4380ddedcc1b7af9bd
Ibrokhimkhuja <ibrohimechanic@gmail.com>
2024-06-17 20:50:48 +0500

    (feat) add routing and refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./pages/login/login.component";
+import {AuthGuard} from "@auth/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todos',
+    canMatch: [AuthGuard],
+    loadChildren: () => import('./pages/todo/todo.module').then((m) => m.TodoModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then(
+        (m) => m.LoginComponent
+      ),
+    title: 'Payme Todo | Login',
+  },
+  {
+    path: 'not-found',
+    loadComponent: () =>
+      import('./pages/not-found/not-found.component').then(
+        (m) => m.NotFoundComponent,
+      ),
+    title: 'Payme Todo | 404 Not Found',
+  },
+  { path: '**', redirectTo: 'not-found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "@auth/auth.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'payme-todo';
+
+  constructor(
+      private _authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this._authService.getUserTokenFromLocalStorage();
+  }
 }
